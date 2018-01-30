a63b14d0b2e4572b6a404705fdfd90e6b212ddf8
Elyse Goldblum <egoldblu@umich.edu>
2018-01-30 09:28:47 -0500

    deleted weird lines

--- hello.py
+++ hello.py
@@ -1,8 +1,5 @@
 print ("My name is Charlotte")
 print ("My name is Emily")
 print ("My name is Elyse")
-<<<<<<< HEAD
 print ("I was born in Abington, PA")
-=======
 print ("I was born in New York")
->>>>>>> b68679f7bdde70eb5e48cbbc40b5ac5d47efca1a
