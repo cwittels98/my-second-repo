258aabca54a84b1d2c724cb32086d62592ba50b1
Elyse Goldblum <egoldblu@umich.edu>
2018-01-30 09:34:43 -0500

    edited file

--- hello.py
+++ hello.py
@@ -3,5 +3,4 @@ print ("My name is Emily")
 print ("My name is Elyse")
 print ("I was born in Abington, PA")
 print ("I was born in New York")
->>>>>>> b68679f7bdde70eb5e48cbbc40b5ac5d47efca1a
 print ("I was also born in New York")
